70f8838cf6e04ae1561b116fc2120d2e50d30764
Johan Nyberg <johan.nyberg@hotmail.se>
2024-02-24 17:14:16 +0100

    first commit

---
+++ gamelet.js
@@ -0,0 +1,27 @@
+const ball = document.getElementById("ball");
+document.addEventListener("keydown", handleKeyPress);
+let position = 0;
+let verticalPosition = 0;
+
+function handleKeyPress(e) {
+  if (e.code === "ArrowLeft") {
+    position = position - 10;
+  }
+  if (e.code === "ArrowRight") {
+    position = position + 10;
+  }
+  if (e.code === "ArrowDown") {
+    verticalPosition = verticalPosition + 10;
+  }
+  if (e.code === "ArrowUp") {
+    verticalPosition = verticalPosition - 10;
+  }
+  if (position < 0) {
+    position = 0;
+  }
+  refresh(); //Updates the balls position
+}
+function refresh() {
+  ball.style.left = position + "px";
+  ball.style.top = verticalPosition + "px";
+}
